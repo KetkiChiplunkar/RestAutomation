9ad3d306cc03ccda65536537ec3e2d4fd5884c44
KetkiChiplunkar <33939111+KetkiChiplunkar@users.noreply.github.com>
2019-04-13 13:54:59 +0530

    Updating runner
    Updating runner

--- src/test/java/runner/RunCukes.java
+++ src/test/java/runner/RunCukes.java
@@ -4,13 +4,15 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
-        plugin={"pretty","json:target/RunCuke/cucmber_repot.json"
-        		,"html:target/RunCuke/cucumber-html-report"},
+        plugin={"pretty","json:target/RunCuke/cucmber_report.json"
+        		,"html:target/RunCuke/cucumber-html-report","html:target/site/cucumber-pretty"
+        		,"json:target/cucumber.json"},
         features = "src\\test\\resources\\Feature",
         		
         glue="stepDefinition",
-       tags={"@Paypal_RestAssured,@Paypal_CreateOrder"}
+       tags={"@Paypal_CreateOrder"}
         )
+
 
 public class RunCukes extends AbstractTestNGCucumberTests {
 	
